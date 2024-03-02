539de1da54c69153a1879f6d909a12c4553be105
Adrian-TJ <atrevino136@gmail.com>
2024-03-02 16:22:02 -0600

    Mejoras de UI registro y login

--- nexusCrusaders/src/components/Login.tsx
+++ nexusCrusaders/src/components/Login.tsx
@@ -4,25 +4,21 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    
-    <div className="container">
-      <div className="nav">
-        <h1> Impulsa tu aprendizaje </h1>
-      </div>
-      <div className="login">
-        <form>
+    <div className="loginContainer">
+      <div className="flexCenter loginDiv">
+        <form className="loginForm">
+          <div className='flexCenter'>
+            <img id="logo" src="./public/logo.svg" alt="Nexus logo"/>
+            <h1>Nexus Crusaders</h1>
+          </div>
           <div className="email">
             <input type="email" name="email" id="email" placeholder="Email" />
           </div>
-
           <div className="password">
             <input type="password" placeholder="Password" />
           </div>
-
-          <div className="log in">
-            <Link to='/registro'><button> Registrarse </button></Link>
-             <button > entrar </button>
-          </div>
+          <button className="flexCenter iniciarSesion" type="submit">Iniciar Sesion</button>
+          <Link className='login' to='/registro'>Registrarse</Link>
         </form>
       </div>
     </div>
