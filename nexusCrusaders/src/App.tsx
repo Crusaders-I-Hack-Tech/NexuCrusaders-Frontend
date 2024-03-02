6e4ef26ac4fba45d963c7db633a3652f1672e49c
Pedro San <pedrosava01@gmail.com>
2024-03-02 16:59:02 -0600

    [React][navigate] : Navigation between pages and login Logic

--- nexusCrusaders/src/App.tsx
+++ nexusCrusaders/src/App.tsx
@@ -1,15 +1,15 @@
-import { useState,useEffect } from 'react'
-import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login'
-import Registro from './components/Registro'
+import LandingPage from './components/LandingPage';
+import Login from './components/Login';
+import Registro from './components/Registro';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/registro" element={<Registro />} />
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </Router>
   );
--- nexusCrusaders/src/components/Login.tsx
+++ nexusCrusaders/src/components/Login.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../styles/login.css";
 import { Link } from "react-router-dom";
 
-
 const Login = () => {
-  return (
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const checkCredentials = (email:string, password:string) => {
     
+    return email == 'user@example.com' && password == 'password';
+  }
+
+  const handleLogin = () => {
+    if (checkCredentials(email, password)) {
+      navigate('/');
+    } else {
+      alert('Invalid credentials');
+    }
+  };
+
+  return (
     <div className="container">
       <div className="nav">
         <h1> Impulsa tu aprendizaje </h1>
@@ -12,16 +29,24 @@ const Login = () => {
       <div className="login">
         <form>
           <div className="email">
-            <input type="email" name="email" id="email" placeholder="Email" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
 
           <div className="password">
-            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
           </div>
 
           <div className="log in">
-            <Link to='/registro'><button> Registrarse </button></Link>
-             <button > entrar </button>
+            <Link to="/registro">
+              <button> Registrarse </button>
+            </Link>
+            <button onClick={handleLogin}> entrar </button>
           </div>
         </form>
       </div>
